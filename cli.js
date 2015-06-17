b7e70f4b6fa1b446035ed5f33227416229b54956
Erick Zanardo <erickzanardoo@gmail.com>
2015-06-17 13:45:53 -0300

    adding support for files

--- cli.js
+++ cli.js
@@ -1,7 +1,23 @@
 #!/usr/bin/env node
 
 var args = process.argv;
-var text = args[args.length - 1];
 
 var encoder = require('./index.js');
-process.stdout.write(encoder(text));
+if(args.indexOf('-f') != -1) {
+  var i = args.indexOf('-f');
+  args.splice(i, 1);
+
+  var filename = args[args.length - 1];
+
+  var fs = require('fs');
+  fs.readFile(filename, 'utf8', function(err, text) {
+    if(err) {
+      throw err;
+    }
+    process.stdout.write(encoder(text));
+  })
+} else {
+  var text = args[args.length - 1];
+  process.stdout.write(encoder(text));
+}
+
